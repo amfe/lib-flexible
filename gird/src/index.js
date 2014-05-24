cc0722877ef8872b249ee1aaaf63cf97e82a402b
寒泉 <hanquan.csf@taobao.com>
2014-05-24 15:23:37 +0800

    remove grid folder(conetent moved)

--- gird/src/index.js
+++
@@ -1,9 +0,0 @@
-;
-(function(win) {
-    function setUnitA(){
-        document.documentElement.style.fontSize = document.documentElement.clientWidth / 16 +"px";
-    }
-    var h = null;
-    window.addEventListener("resize",function(){clearTimeout(h);h=setTimeout(setUnitA,300);},false);
-    setUnitA();
-})(window);
