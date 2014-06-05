6e7266fedf630902251d199b4a3c7df24325e32e
zhuxun.jb <zhuxun.jb@taobao.com>
2014-06-05 16:37:30 +0800

    更新dpr方案，字体设置px的方案

--- src/tbm.js
+++ src/tbm.js
@@ -1,27 +1,24 @@
 ;(function(win) {
     var h;
-    var scale = 1;
-    var dpr = 1;
-    if (win.devicePixelRatio === 2 && win.navigator.appVersion.match(/iphone/gi)) {
-        scale = 0.5;
-        dpr = 2;
-    }
+    var dpr = win.navigator.appVersion.match(/iphone/gi)?win.devicePixelRatio:1;
+    var scale = 1 / dpr;
 
     function setUnitA(){
-        clearTimeout(h);
-        h = setTimeout(function() {
-            win.rem = window.innerWidth / 16;
-            document.documentElement.style.fontSize = win.rem + 'px';
-        }, 300);
+        win.rem = window.innerWidth / 16;
+        document.documentElement.style.fontSize = win.rem + 'px';
     }
 
     win.dpr = dpr;
-    win.addEventListener('resize', setUnitA, false);
+    win.addEventListener('resize', function() {
+        clearTimeout(h);
+        h = setTimeout(setUnitA, 300);
+    }, false);
 
     document.write('<meta name="viewport" content="initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no"/>');
     document.addEventListener('DOMContentLoaded', function(){
+        setUnitA();
         document.body.setAttribute('data-dpr', dpr);
         document.body.style.fontSize = 12 * dpr + 'px';
     }, false);
-    setUnitA();
+    
 })(window);
