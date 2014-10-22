5e8df45572132113cc154510ba5421aa04e38564
zhuxun.jb <zhuxun.jb@taobao.com>
2014-10-22 14:54:08 +0800

    增加css的js编译

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,6 +83,14 @@ module.exports = function(grunt) {
             }
         },
 
+        css2js: {
+            main: {
+                files: {
+                    '<%= distPath %>/<%= name %>_css.js': ['<%= distPath %>/<%= name %>.css']
+                }
+            }
+        },
+
 
         watch: {
             combo: {
@@ -126,6 +134,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-depconcat');
     grunt.loadNpmTasks('grunt-depcombo');
+    grunt.loadNpmTasks('grunt-css2js');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -133,7 +142,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('dist', ['clean', 'copy', 'depconcat', 'uglify', 'less', 'cssmin', 'depcombo']);
+    grunt.registerTask('dist', ['clean', 'copy', 'depconcat', 'uglify', 'less', 'cssmin', 'css2js', 'depcombo']);
     grunt.registerTask('dev', ['watch']);
 
     grunt.registerTask('default', ['dist']);
