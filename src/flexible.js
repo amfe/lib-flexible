7c06a10175a6f743a23d9f4ee86df3b4245f5e66
周祥 <bob.zhouxiang@gmail.com>
2016-11-16 11:38:21 +0800

    删除无效判断
    Line 34
    dpr 值在第一行就已经被限制死了，为 false 的情况才能进入条件内，那么在条件内确定 dpr 值的时候有 devicePixelRatio 的判断就已经足够了。

--- src/flexible.js
+++ src/flexible.js
@@ -37,9 +37,9 @@
         var devicePixelRatio = win.devicePixelRatio;
         if (isIPhone) {
             // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
-            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {                
+            if (devicePixelRatio >= 3) {                
                 dpr = 3;
-            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)){
+            } else if (devicePixelRatio >= 2){
                 dpr = 2;
             } else {
                 dpr = 1;
